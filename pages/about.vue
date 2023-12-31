00234c0dad50b95ff323bd312d8676c455fe3d33
kim-heesu <cjmgmltn@naver.com>
2023-12-31 22:42:13 +0900

    ㅇㅇ

--- pages/about.vue
+++ pages/about.vue
@@ -11,7 +11,7 @@ export default {
         { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt Movie App' },
         { hid: 'og:title', property: 'og:title', content: '여긴 어바웃 페이지' },
         { hid: 'og:description', property: 'og:description', content: 'SEO이슈 땜에 생성했으니까 해결 잘 되나 확인은 해봐야겠쬬..?' },
-        { hid: 'og:url', property: 'og:url', content:`${process.envCLIENT_URL}${this.$route.fullPath}`}
+        { hid: 'og:url', property: 'og:url', content:`${process.env.CLIENT_URL}${this.$route.fullPath}`}
       ]
     }
   }
